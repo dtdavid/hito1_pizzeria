aa256f4d48033be07a84ef09b2f96382ffc3122a
david.docampo <david.docampo@gmail.com>
2024-11-16 11:02:39 +0100

    ajustes tailwindcss

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -7,9 +7,10 @@ const Header = () => {
     style = {{
       backgroundImage: "url('./img/Header.jpg ')"
     }}>
-      <div className = "flex flex-col justify-center items-center h-full text-white">
+      <div className = "flex flex-col justify-center items-center h-full text-white ">
       <h1 className = "text-4xl font-bold">¡Pizzería Mamma Mía!</h1>
       <p className = "text-xs">¡Tenemos las mejores pizzas que podrás encontrar!</p>
+      <hr className="border-t border-gray-300 w-1/2 mt-4"/>
       </div>
     </header>
   )
