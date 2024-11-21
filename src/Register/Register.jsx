e53409b9536c351cd9a6af44bde2f4b314870c49
david.docampo <david.docampo@gmail.com>
2024-11-21 18:00:05 +0100

    validaciones en el password y limpieza del formulario

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -4,16 +4,35 @@ const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPas, setConfirmPas] = useState('')
-    const [submit, setSubmit] = useState([])
+    //const [submit, setSubmit] = useState([])
 
      const validar = (e) => {
+        console.log(email, password, confirmPas)
         e.preventDefault() //previene el envío automático del formulario
-        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]$/
-        if(email.trim() =='' || !regexEmail.test(email)) {
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        const regexPassword = /^.{6,}$/
+        console.log(email)
+        if(email.trim() ==='' || !regexEmail.test(email)) {
             alert ("Campo email vacío o dirección no válida")
-        } else if (password.trim() == '' || confirmPas.trim() == '' ) {
-            alert("campos password o confirmar password vacíos")
+            return
         }
+        if (password.trim() === '' || confirmPas.trim() === '' ) {
+            alert("campos password o confirmar password vacíos")
+            return
+        }
+        if (!regexPassword.test(password)) {
+            alert("campo mínimo de 6 caractéres!")
+            return
+        }
+        if(password !== confirmPas) {
+            alert("el password no está confirmado")
+            return
+        }
+        alert("¡ SEND SUCCESFULL!")
+
+        setEmail('')
+        setPassword('')
+        setConfirmPas('')
      }
     
   return (
@@ -27,21 +46,22 @@ const Register = () => {
           </h2>
         </div>
 
-        {/* Formulario */}
+        {/* ################################Formulario ####################################*/}
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" /*method="POST"*/ className="space-y-6" onSubmit = {validar}> 
 
-            {/*input email */}
+            {/*###############################input email ###################################*/}
 
             <div>
               <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
-                <h3>{email}</h3>
+                {/*<h3>{email}</h3>*/}
                 <input
                   onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   id="email"
                   name="email"
                   type= "text" /*"email"*/
@@ -52,7 +72,7 @@ const Register = () => {
                 />
               </div>
             </div>
-            {/*Input password*/}
+            {/*#####################Input password#########################*/}
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
@@ -61,12 +81,13 @@ const Register = () => {
 
               </div>
               <div className="mt-2">
-                <h3>{password}</h3>
+                {/*<h3>{password}</h3>*/} 
                 <input
+                  value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   id="password"
                   name="password"
-                  type= "text"   /* "password"*/
+                  type= "password"
                   //required
                   autoComplete="current-password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 pl-2"
@@ -74,32 +95,32 @@ const Register = () => {
               </div>
             </div>
 
-            {/* Input confirm password*/}
+            {/*############################ Input confirm password#######################################*/}
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-gray-900">
                   Confirm Password
                 </label>
 
               </div>
               <div className="mt-2">
-                <h3>{confirmPas}</h3>
+                
                 <input
+                  value={confirmPas}
                   onChange={(e) => setConfirmPas(e.target.value)}
-                  id="password"
-                  name="password"
-                  type= "text" /*"password"*/
+                  id="confirm-password"
+                  name="confirm-password"
+                  type= "confirm-password"
                   //required
                   autoComplete="current-password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 pl-2"
                 />
               </div>
             </div>
-             {/*Input Sign in */}
+             {/*#############################Input Sign in######################################## */}
             <div>
                 {/* <h3>{submit}</h3> */}
               <button
-                // onClick={() => setSubmit(handleSubmit)}
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
