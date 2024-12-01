96eb4dbb55edc736fd5ec3994e5dfe87baabcd32
david.docampo <david.docampo@gmail.com>
2024-12-01 21:59:43 +0100

    arreglar la función handleQuitar()

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import {pizzaCart} from '../data/pizzas'
 import {format} from '../utils/format'
-import { useState } from 'react'
+
 
 const Cart = () => {
 
     const [cart, setCart] = useState(pizzaCart)
-    const handleAgregar = (indice) => {
-        cart[indice].count++
-        setCart([...cart])
+    const [totalPrice, setTotalPrice] = useState(0)
+    //console.log(cart)
+
+    const handleAgregar = (id) => {
+        // cart[indice].count++
+        // setCart([...cart])
+        // cambiamos trabajando con prev
+
+        setCart((prevCart) => 
+            prevCart.map((pizza) =>  /*utilizo prevCart y lo mapeo para crear
+         un nuevo objeto cambiando la key count si se modifica*/
+            pizza.id === id ? {...pizza, count: pizza.count + 1} : pizza)
+        )
     }
-    const handleQuitar = (indice) => {
-        cart[indice].count--
-        {/*Este método, aunque elimina pizzas del array, no elimina la pizza que se está bajando a 0, no funciona bien */}
-        setCart([...cart.filter((pizza) => pizza.count > 0)])
+    const handleQuitar = (id) => {
+        // cart[indice].count--
+        // {/*Este método, aunque elimina pizzas del array, no elimina la pizza que se está bajando a 0, no funciona bien */}
+        // setCart([...cart.filter((pizza) => pizza.count > 0)])
+        setCart((prevCart) => 
+            prevCart.map((pizza) =>
+            pizza.id === id ? {...pizza, count: pizza.count -1} : pizza
+            ).filter((pizza) => pizza.count > 0) //aquí filtramos que las pizzas tengan el contador por encima de 0
+        )
     }
     {/* recorremos el estado del carrito (cart) con el método reduce, para sumar "0" ("sumar" hace de acumulador y su valor inicial es "0
         ") con el precio de la pizza * el
         número de pizzas que eligió el usuario, que debería ser como mínimo "1" y 
         llamamos a la función desde el h1 donde se muestra "Total: $" */ }
-    const total = () => {
-        return cart.reduce((suma, pizza) => suma + pizza.price * pizza.count, 0)
-    }
+
+        //No se renderiza bien o no se actualiza junto con los cambios en el carro. Voy a probar con useMemo
+    
+        const total = useMemo(() => {
+            return cart.reduce((suma, pizza) => suma + pizza.price * pizza.count, 0)
+    }, [cart])
+
+        useEffect(() => {
+        setTotalPrice(total)
+    }, [total])
+    //console.log(total)
+
     {/* Cambiamos a mayúscula la primera letra y llamamos a la función desde el párrafo con el pizza.name o copiamos esta línea directamente en el párrafo. */}
     const capitalize = (pizza) => {
         return pizza.charAt(0).toUpperCase() + pizza.slice(1).toLowerCase()
@@ -31,8 +55,8 @@ const Cart = () => {
     <div className="max-w-xl mx-auto px-4">
     <ul role="list" className="divide-y divide-gray-100 flex flex-col items-start px-4">
         <h2 className="text-xl font-semibold text-gray-900 mt-10">Detalles del pedido:</h2>
-        {cart.map((pizza, index) => (
-        <li key={index} className="flex justify-between w-full py-5 items-center">
+        {cart.map((pizza) => (
+        <li key={pizza.id} className="flex justify-between w-full py-5 items-center">
                        {/*pizza.img y pizza.name */}
             <div className="flex items-center gap-x-4">
                 <img 
@@ -44,13 +68,13 @@ const Cart = () => {
                 {/*precio - botones - contador */}
             <div className="flex items-center gap-x-4">
                 <p className="text-sm font-semibold">${format(pizza.price)}</p>
-                <button className="w-8 h-8 border rounded-md border-red-600 text-red-600 text-lg items-center justify-center" onClick={() => handleQuitar(index)}>-</button>
-                <span key= {index} className="text-sm font-semibold">{pizza.count}</span>
-                <button className="w-8 h-8 border  rounded border-blue-600 text-blue-600 text-lg items-center justify-center" onClick={() => handleAgregar(index)}>+</button>
+                <button className="w-8 h-8 border rounded-md border-red-600 text-red-600 text-lg items-center justify-center" onClick={() => handleQuitar(pizza.id)}>-</button>
+                <span key= {pizza.id} className="text-sm font-semibold">{pizza.count}</span>
+                <button className="w-8 h-8 border  rounded border-blue-600 text-blue-600 text-lg items-center justify-center" onClick={() => handleAgregar(pizza.id)}>+</button>
             </div>
         </li>
         ))}
-        <h1 className = "text-xl font-bold mt-5">Total: ${format(total())}</h1>
+        <h1 className = "text-xl font-bold mt-5">Total: ${format(totalPrice)}</h1>
         <button className = "mt-2 px-6 py-2 border rounded-md text-center text-white bg-black ">Pagar</button>
     </ul>
     </div>
