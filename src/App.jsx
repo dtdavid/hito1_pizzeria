0e5ec64ec7dad7135d3ed3b9885b5d7832b16e67
david.docampo <david.docampo@gmail.com>
2024-12-11 11:29:42 +0100

    trasladar a la carpeta pages Register y Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Home from './pages/Home'
 import Footer from './Footer/Footer'
 import Pizza from './pages/Pizza'
 //import './index.css'
-//import Cart from './pages/Cart'
-//import Register from './Register/Register'
-//import Login from './Login/Login'
+import Cart from './pages/Cart'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   
@@ -15,11 +15,11 @@ function App() {
     <div className = "w-full flex flex-col min-h-screen justify-center items-center">
       <Navbar />
       <div className = "flex-grow flex justify-center items-center w-full">
-      {/* <Pizza /> */}
+      <Pizza /> 
       <Home />
-      {/* <Cart /> */}
-      {/*<Register/>*/}
-      {/*<Login />*/}
+      <Cart />
+      <Register/>
+      <Login/>
       </div>
       <Footer />
       
